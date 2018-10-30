12a52bdd527138a2e270f18e53dcc936ff563641
Lovac42 <lovac42@protonmail.com>
2018-10-29 21:29:37 -0400

    version bump

--- src/slackersDelight.py
+++ src/slackersDelight.py
@@ -2,7 +2,7 @@
 # Copyright: (C) 2018 Lovac42
 # Support: https://github.com/lovac42/SlackersDelight
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Version: 0.0.1
+# Version: 0.0.2
 
 
 # == User Config =========================================
